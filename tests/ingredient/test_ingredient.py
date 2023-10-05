e2bbc337b17210f800817e557a669262312beda1
Luanderson Alves <luaoderson@gmail.com>
2023-10-05 16:38:04 -0300

    test: ingredient class

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,28 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    queijo_muss_ing = Ingredient('queijo mussarela')
+    bacon = Ingredient('bacon')
+
+    assert queijo_muss_ing.name == "queijo mussarela"
+
+    assert queijo_muss_ing.__repr__() == "Ingredient('queijo mussarela')"
+
+    assert (queijo_muss_ing == bacon) == False
+
+    assert (queijo_muss_ing == queijo_muss_ing) == True
+
+    assert queijo_muss_ing.__hash__() != bacon.__hash__()
+
+    assert queijo_muss_ing.__hash__() == queijo_muss_ing.__hash__()
+
+    expected_restrictions_queijo_muss = {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert queijo_muss_ing.restrictions == expected_restrictions_queijo_muss
+    
