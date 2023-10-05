2b2c748f48ffcbf531dee234c17b477851b91453
Luanderson Alves <luaoderson@gmail.com>
2023-10-05 17:00:25 -0300

    test: dish class

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,41 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from models.ingredient import Ingredient
+from models.ingredient import Restriction
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    dish_strogonof = Dish("Strogonof", 15.00)
+    dish_salad = Dish("Salad", 11.00)
+
+    assert dish_strogonof.name == "Strogonof"
+
+    assert dish_strogonof != dish_salad
+
+    assert dish_strogonof == dish_strogonof
+
+    assert (dish_strogonof.__hash__() != dish_salad.__hash__())
+
+    assert (dish_strogonof.__hash__() == dish_strogonof.__hash__())
+
+    with pytest.raises(TypeError):
+        Dish("Strogonof", "15")
+
+    with pytest.raises(ValueError):
+        Dish("Strogonof", -1)
+
+    assert dish_strogonof.__repr__() == "Dish('Strogonof', R$15.00)"
+
+    queijo_muss_ing = Ingredient("queijo mussarela")
+
+    dish_strogonof.add_ingredient_dependency(queijo_muss_ing, 5)
+
+    assert dish_strogonof.get_ingredients() == {queijo_muss_ing}
+
+    expected_restrictions_queijo_muss = {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+
+    assert dish_strogonof.get_restrictions() == expected_restrictions_queijo_muss
